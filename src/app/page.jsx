df8f2550a067dab93fa1d565de4da55fed1f92e7
nirajanchaudhary00001 <nirajanchaudhary00001@gmail.com>
2025-11-14 16:04:32 +0545

    test

---
+++ src/app/page.jsx
@@ -0,0 +1,14 @@
+import CTA from "@/components/CTA";
+import Hero from "@/components/Hero";
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <Hero />
+      <CTA />
+    </div>
+  );
+};
+
+export default page;
